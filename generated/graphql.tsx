b7a3f69432dc2e2bfae295b337ff635daddebbc7
Nguyễn Đắc Chí <chind@hasaki.vn>
2021-11-11 00:27:56 +0700

    change password gui

--- generated/graphql.tsx
+++ generated/graphql.tsx
@@ -22,6 +22,7 @@ export type FieldError = {
 
 export type Mutation = {
   __typename?: 'Mutation';
+  changePassword: UserResponse;
   createPost: Post;
   deletePost: Scalars['Boolean'];
   forgotPassword: Scalars['Boolean'];
@@ -32,6 +33,12 @@ export type Mutation = {
 };
 
 
+export type MutationChangePasswordArgs = {
+  newPassword: Scalars['String'];
+  token: Scalars['String'];
+};
+
+
 export type MutationCreatePostArgs = {
   title: Scalars['String'];
 };
@@ -39,6 +46,11 @@ export type MutationCreatePostArgs = {
 
 export type MutationDeletePostArgs = {
   id: Scalars['Int'];
+};
+
+
+export type MutationForgotPasswordArgs = {
+  email: Scalars['String'];
 };
 
 
@@ -99,7 +111,19 @@ export type UsernamePasswordInput = {
   username: Scalars['String'];
 };
 
+export type RegularErrorFragment = { __typename?: 'FieldError', field: string, message: string };
+
 export type RegularUserFragment = { __typename?: 'User', id: number, username: string };
+
+export type RegularUserResponseFragment = { __typename?: 'UserResponse', errors?: Maybe<Array<{ __typename?: 'FieldError', field: string, message: string }>>, user?: Maybe<{ __typename?: 'User', id: number, username: string }> };
+
+export type ChangePasswordMutationVariables = Exact<{
+  newPassword: Scalars['String'];
+  token: Scalars['String'];
+}>;
+
+
+export type ChangePasswordMutation = { __typename?: 'Mutation', changePassword: { __typename?: 'UserResponse', errors?: Maybe<Array<{ __typename?: 'FieldError', field: string, message: string }>>, user?: Maybe<{ __typename?: 'User', id: number, username: string }> } };
 
 export type LoginMutationVariables = Exact<{
   usernameOrEmail: Scalars['String'];
@@ -131,25 +155,47 @@ export type ProfileQueryVariables = Exact<{ [key: string]: never; }>;
 
 export type ProfileQuery = { __typename?: 'Query', profile?: Maybe<{ __typename?: 'UserResponse', errors?: Maybe<Array<{ __typename?: 'FieldError', field: string, message: string }>>, user?: Maybe<{ __typename?: 'User', id: number, username: string }> }> };
 
+export const RegularErrorFragmentDoc = gql`
+    fragment RegularError on FieldError {
+  field
+  message
+}
+    `;
 export const RegularUserFragmentDoc = gql`
     fragment RegularUser on User {
   id
   username
 }
     `;
+export const RegularUserResponseFragmentDoc = gql`
+    fragment RegularUserResponse on UserResponse {
+  errors {
+    ...RegularError
+  }
+  user {
+    ...RegularUser
+  }
+}
+    ${RegularErrorFragmentDoc}
+${RegularUserFragmentDoc}`;
+export const ChangePasswordDocument = gql`
+    mutation ChangePassword($newPassword: String!, $token: String!) {
+  changePassword(newPassword: $newPassword, token: $token) {
+    ...RegularUserResponse
+  }
+}
+    ${RegularUserResponseFragmentDoc}`;
+
+export function useChangePasswordMutation() {
+  return Urql.useMutation<ChangePasswordMutation, ChangePasswordMutationVariables>(ChangePasswordDocument);
+};
 export const LoginDocument = gql`
     mutation Login($usernameOrEmail: String!, $password: String!) {
   login(usernameOrEmail: $usernameOrEmail, password: $password) {
-    errors {
-      field
-      message
-    }
-    user {
-      ...RegularUser
-    }
+    ...RegularUserResponse
   }
 }
-    ${RegularUserFragmentDoc}`;
+    ${RegularUserResponseFragmentDoc}`;
 
 export function useLoginMutation() {
   return Urql.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument);
@@ -166,16 +212,10 @@ export function useLogoutMutation() {
 export const RegisterDocument = gql`
     mutation Register($options: UsernamePasswordInput!) {
   register(options: $options) {
-    errors {
-      field
-      message
-    }
-    user {
-      ...RegularUser
-    }
+    ...RegularUserResponse
   }
 }
-    ${RegularUserFragmentDoc}`;
+    ${RegularUserResponseFragmentDoc}`;
 
 export function useRegisterMutation() {
   return Urql.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument);
---
+++ pages/change-password/[token].tsx
@@ -0,0 +1,88 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { GetServerSideProps, NextPage } from 'next';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import InputField from '../../components/InputField';
+import * as yup from 'yup';
+import styles from './change-password.module.css';
+import { Button } from '@chakra-ui/button';
+import { useChangePasswordMutation } from '../../generated/graphql';
+import { toErrorMap } from '../../utils/toErrorMap';
+import { useRouter } from 'next/dist/client/router';
+import { Box } from '@chakra-ui/layout';
+import { withUrqlClient } from 'next-urql';
+import { createUrqlClient } from '../../utils/createUrqlClient';
+
+interface FormData {
+  newPassword: string;
+}
+
+const ChangePassword: NextPage<{ token: string }> = ({ token }) => {
+  const [isSubmiting, setIsSubmiting] = useState<boolean>(false);
+  const [responseErrors, setResponseErrors] = useState<Record<
+    string,
+    string
+  > | null>(null);
+
+  const [{}, changePassword] = useChangePasswordMutation();
+
+  const router = useRouter();
+
+  const schema = yup.object().shape({
+    newPassword: yup
+      .string()
+      .required('New password is required!')
+      .min(3, 'Password length must larger than 2!'),
+  });
+
+  const {
+    register: registerForm,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = async ({ newPassword }: FormData) => {
+    const response = await changePassword({ token, newPassword });
+    if (response?.data?.changePassword?.errors) {
+      const errorMap = toErrorMap(response?.data?.changePassword?.errors);
+      setResponseErrors(errorMap);
+    } else {
+      router.push('/');
+    }
+  };
+
+  return (
+    <form className={styles.container} onSubmit={handleSubmit(onSubmit)}>
+      <InputField
+        displayName="New Password"
+        fieldName="newPassword"
+        fieldType="password"
+        errors={errors}
+        placeholder="Enter your new password!"
+        register={registerForm}
+        responseError={responseErrors?.newPassword}
+      />
+      <Box color="red">{responseErrors?.token}</Box>
+      <Button
+        mt={4}
+        colorScheme="facebook"
+        type="submit"
+        isLoading={isSubmiting}
+      >
+        Change Password
+      </Button>
+    </form>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  return {
+    props: {
+      token: query.token,
+    },
+  };
+};
+
+export default withUrqlClient(createUrqlClient, { ssr: false })(ChangePassword);
